aeaea61e3223b5911610efedf6d5fc1f4fdf856c
inforoutes59 <inforoutes.lenord@gmail.com>
2024-04-09 11:18:33 +0200

    add zoom on click

--- src/components/map.component.js
+++ src/components/map.component.js
@@ -191,6 +191,7 @@ function MapComponent() {
                 .setLatLng([feature.geometry.coordinates[0][lengthCoord][1], feature.geometry.coordinates[0][lengthCoord][0]])
                 .setContent(popupContent)
                 .openOn(map);
+            mapRef.current.setView([feature.geometry.coordinates[0][lengthCoord][1], feature.geometry.coordinates[0][lengthCoord][0]], 12);
             mapRef.current.on('popupclose', () => {
                 if (highlightedDeviationLayer) {
                     mapRef.current.removeLayer(highlightedDeviationLayer);
@@ -245,6 +246,8 @@ function MapComponent() {
                 .setLatLng(latLng)
                 .setContent(popupContent)
                 .openOn(map);
+            mapRef.current.setView([feature.geometry.coordinates[0][lengthCoord][1], feature.geometry.coordinates[0][lengthCoord][0]], 12);
+
         }
     };
 
