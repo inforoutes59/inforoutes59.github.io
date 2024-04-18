47c477f0a5183a8ae027ef1c63563a325530196e
inforoutes59 <inforoutes.lenord@gmail.com>
2024-04-18 09:29:40 +0200

    reset zoom arrondissement

--- src/components/legend.component.js
+++ src/components/legend.component.js
@@ -2,10 +2,10 @@ import React from 'react';
 import L from 'leaflet';
 import { useMap } from 'react-leaflet';
 
-function Legend() {
+function Legend({arrondissements}) {
     const map = useMap();
 
-    // Fonction pour générer la légende HTML
+    // Fonction pour générer la légende HTML et gérer les clics
     const createLegend = () => {
         const legendDiv = L.DomUtil.create('div', 'legend');
         legendDiv.style.backgroundColor = 'white';
@@ -14,14 +14,39 @@ function Legend() {
         legendDiv.style.padding = '0.2vh';
         legendDiv.innerHTML = `
             <div>Arrondissements :</div>
-            <div><span style="background-color: #ebd834; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Avesnes</div>
-            <div><span style="background-color: #34eb71; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Cambrai</div>
-            <div><span style="background-color: #eb34e8; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Douai</div>
-            <div><span style="background-color: #eb3434; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Dunkerque</div>
-            <div><span style="background-color: #345eeb; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Valenciennes</div>
-            <div><span style="background-color: grey; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>MEL</div>
+            <div id="avesnes" class="arrondissement" style="cursor: pointer;"><span style="background-color: #ebd834; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Avesnes</div>
+            <div id="cambrai" class="arrondissement" style="cursor: pointer;"><span style="background-color: #34eb71; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Cambrai</div>
+            <div id="douai" class="arrondissement" style="cursor: pointer;"><span style="background-color: #eb34e8; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Douai</div>
+            <div id="dunkerque" class="arrondissement" style="cursor: pointer;"><span style="background-color: #eb3434; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Dunkerque</div>
+            <div id="valenciennes" class="arrondissement" style="cursor: pointer;"><span style="background-color: #345eeb; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>Valenciennes</div>
+            <div id="mel" class="arrondissement" style="cursor: pointer;"><span style="background-color: grey; width: 2em; height: 2em; display: inline-block; margin-right: 0.5em;"></span>MEL</div>
         `;
+
+        // Gestion des clics sur les arrondissements
+        legendDiv.querySelectorAll('.arrondissement').forEach(item => {
+            item.addEventListener('click', () => {
+                const arrondissement = item.id;
+                const bounds = getBoundsOfArrondissement(arrondissement);
+                if (bounds) {
+                    map.fitBounds(bounds);
+                }
+            });
+        });
+
         return legendDiv;
+    };
+
+    // Fonction pour extraire les limites géographiques de l'arrondissement à partir du GeoJSON
+    const getBoundsOfArrondissement = (arrondissement) => {
+        const features = arrondissements.features;
+        for (let i = 0; i < features.length; i++) {
+            const properties = features[i].properties;
+            if (properties.Name.toLowerCase().includes(arrondissement.toLowerCase())) {
+                const bounds = L.geoJSON(features[i]).getBounds();
+                return bounds;
+            }
+        }
+        return null;
     };
 
     // Ajouter la légende à la carte
